fce6c0c124ece7df5b33e3d4f0657453b626487d
sebastiandelaroche <sebastian.delaroche.dev@gmail.com>
2022-02-23 11:21:11 -0500

    foobar

--- packages/domain/lib/index.ts
+++ packages/domain/lib/index.ts
@@ -2,8 +2,8 @@ import 'reflect-metadata';
 
 export * from './decorators';
 export * from './domain';
+export * from './event';
 export * from './exceptions';
 export * from './validators';
 export * from './repository';
 export * from './initializer';
-export * from './event';
