e88f0e4bc8a0228a169f3093a8c6c54c628732f5
sebastiandelaroche <sebastian.delaroche.dev@gmail.com>
2022-03-07 23:56:28 -0500

    feat(domain): handle ids in UniqueEntityID

--- packages/domain/lib/domain/unique-entity-id.ts
+++ packages/domain/lib/domain/unique-entity-id.ts
@@ -1,9 +1,16 @@
+import { isEmptyOrNil } from '@kerthin/utils';
 import { ObjectId } from 'mongodb';
 import { createUniqueID } from '../utils/unique-entity-id';
 import { Identifier } from './identifier';
 
 export class UniqueEntityID extends Identifier<string | ObjectId> {
   constructor(id?: string | ObjectId) {
-    super(id || createUniqueID());
+    super(
+      isEmptyOrNil(id)
+        ? createUniqueID()
+        : ObjectId.isValid(id) && typeof id === 'string'
+          ? new ObjectId(id)
+          : id
+    );
   }
 }
